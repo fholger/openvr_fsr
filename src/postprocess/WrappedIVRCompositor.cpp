d76ebe9833f2fc2c1106444700a5a65ee313cdc0
Holger Frydrych <holger@frydrych.org>
2021-07-03 17:17:57 +0200

    Extract hooks to separate source file

--- src/postprocess/WrappedIVRCompositor.cpp
+++ src/postprocess/WrappedIVRCompositor.cpp
@@ -3,7 +3,6 @@
 #include "WrappedIVRCompositor.h"
 #define no_init_all deprecated
 #include <d3d11.h>
-#include <iostream>
 #include <wrl/client.h>
 #include "fsr/fsr_up.h"
 #include "fsr/fsr_cas.h"
@@ -12,37 +11,9 @@
 
 using Microsoft::WRL::ComPtr;
 
-namespace {
-	void helper() {}
-}
-
 namespace vr {
-	std::string GetDllPath() {
-		char path[FILENAME_MAX];
-		HMODULE hm = nullptr;
-
-		void *address = helper;		
-
-		if (!GetModuleHandleExA(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS | GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT, (LPCSTR)address, &hm)) {
-			return ".";
-		}
-
-		GetModuleFileNameA(hm, path, sizeof(path));
-
-		std::string p = path;
-		return p.substr(0, p.find_last_of('\\'));
-	}
-
-	std::ostream& log() {
-		try {
-			static std::ofstream logFile (GetDllPath() + "\\openvr_mod.log");
-			return logFile;
-		} catch (...) {
-			return std::cout;
-		}
-	}
-
 	DXGI_FORMAT TranslateTypelessFormats(DXGI_FORMAT format) {
+		Log() << "Mapping format " << std::hex << format << std::dec << std::endl;
 		switch (format) {
 		case DXGI_FORMAT_R32G32B32A32_TYPELESS:
 			return DXGI_FORMAT_R32G32B32A32_FLOAT;
@@ -54,6 +25,8 @@ namespace vr {
 			return DXGI_FORMAT_R10G10B10A2_UINT;
 		case DXGI_FORMAT_R8G8B8A8_TYPELESS:
 			return DXGI_FORMAT_R8G8B8A8_UNORM;
+		case DXGI_FORMAT_B8G8R8A8_TYPELESS:
+			return DXGI_FORMAT_B8G8R8A8_UNORM;
 
 		default:
 			return format;
@@ -87,7 +60,7 @@ namespace vr {
 		} sharpenConstants;
 
 		void Create(ID3D11Texture2D *tex) {
-			log() << "Creating resources for FSR\n";
+			Log() << "Creating resources for FSR\n";
 			tex->GetDevice( device.GetAddressOf() );
 			device->GetImmediateContext( context.GetAddressOf() );
 			D3D11_TEXTURE2D_DESC std;
@@ -101,7 +74,7 @@ namespace vr {
 				outputWidth = std.Width * Config::Instance().fsrQuality;
 				outputHeight = std.Height * Config::Instance().fsrQuality;
 			}
-			log() << "Creating output texture of size " << outputWidth << "x" << outputHeight << "\n";
+			Log() << "Creating output texture of size " << outputWidth << "x" << outputHeight << "\n";
 			D3D11_TEXTURE2D_DESC td;
 			td.Width = outputWidth;
 			td.Height = outputHeight;
@@ -115,11 +88,11 @@ namespace vr {
 			td.SampleDesc.Quality = 0;
 			td.ArraySize = 1;
 			if (FAILED(device->CreateTexture2D( &td, nullptr, upscaledTexture.GetAddressOf() ))) {
-				log() << "Failed to create texture.\n";
+				Log() << "Failed to create texture.\n";
 				return;
 			}
 			if (FAILED(device->CreateTexture2D( &td, nullptr, outputTexture.GetAddressOf() ))) {
-				log() << "Failed to create texture.\n";
+				Log() << "Failed to create texture.\n";
 				return;
 			}
 			D3D11_UNORDERED_ACCESS_VIEW_DESC uav;
@@ -137,11 +110,11 @@ namespace vr {
 
 			// create shaders
 			if (FAILED(device->CreateComputeShader( fsr_up_dxbc, fsr_up_dxbc_len, nullptr, fsrUpscaleShader.GetAddressOf() ))) {
-				log() << "Failed to create compute shader.\n";
+				Log() << "Failed to create compute shader.\n";
 				return;
 			}
 			if (FAILED(device->CreateComputeShader( fsr_cas_dxbc, fsr_cas_dxbc_len, nullptr, fsrSharpenShader.GetAddressOf() ))) {
-				log() << "Failed to create compute shader.\n";
+				Log() << "Failed to create compute shader.\n";
 				return;
 			}
 
@@ -165,7 +138,7 @@ namespace vr {
 			upscaleConstants.sourceSize[1] = std.Height;
 			upscaleConstants.sourceSize[2] = 1;
 			upscaleConstants.sourceSize[3] = 1;
-			sharpenConstants.sharpness[0] = Config::Instance().sharpness;
+			sharpenConstants.sharpness[0] = -std::log2(Config::Instance().sharpness);
 			sharpenConstants.sharpness[1] = 1;
 			sharpenConstants.sharpness[2] = 1;
 			sharpenConstants.sharpness[3] = 1;
@@ -181,26 +154,27 @@ namespace vr {
 			init.SysMemSlicePitch = 0;
 			init.pSysMem = &upscaleConstants;
 			if (FAILED(device->CreateBuffer( &bd, &init, upscaleConstantsBuffer.GetAddressOf() ))) {
-				log() << "Failed to create upscale shader constants buffer.\n";
+				Log() << "Failed to create upscale shader constants buffer.\n";
 				return;
 			}
 			bd.ByteWidth = sizeof(SharpenConstants);
 			init.pSysMem = &sharpenConstants;
 			if (FAILED(device->CreateBuffer( &bd, &init, sharpenConstantsBuffer.GetAddressOf() ))) {
-				log() << "Failed to create sharpen shader constants buffer.\n";
+				Log() << "Failed to create sharpen shader constants buffer.\n";
 				return;
 			}
 
-			log() << "Resource creation complete\n";
+			Log() << "Resource creation complete\n";
 			created = true;
 		}
 
 		void Apply(ID3D11Texture2D *tex) {
 			if (inputTextureViews.find(tex) == inputTextureViews.end()) {
-				log() << "Creating shader resource view for input texture " << tex << "\n";
+				Log() << "Creating shader resource view for input texture " << tex << std::endl;
 				// create resource view for input texture
 				D3D11_TEXTURE2D_DESC std;
 				tex->GetDesc( &std );
+				Log() << "Texture has size " << std.Width << "x" << std.Height << "\n";
 				D3D11_SHADER_RESOURCE_VIEW_DESC svd;
 				svd.Format = TranslateTypelessFormats(std.Format);
 				svd.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;
@@ -208,7 +182,7 @@ namespace vr {
 				svd.Texture2D.MipLevels = 1;
 				HRESULT result = device->CreateShaderResourceView( tex, &svd, inputTextureViews[tex].GetAddressOf() );
 				if (FAILED(result)) {
-					log() << "Failed to create resource view: " << std::hex << (unsigned long)result << std::endl;
+					Log() << "Failed to create resource view: " << std::hex << (unsigned long)result << std::dec << std::endl;
 					inputTextureViews.erase( tex );
 					return;
 				}
@@ -241,7 +215,7 @@ namespace vr {
 			if (fsrResources == nullptr) {
 				fsrResources = new FSRRenderResources;
 				fsrResources->Create( texture );
-				log() << std::flush;
+				Log() << std::flush;
 			}
 
 			if (GetTickCount() - fsrResources->lastSwitch > 5000 && eyeCount == 0) {
